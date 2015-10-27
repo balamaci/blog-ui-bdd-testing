b80f7676eb0ddb1b9f81cc321984c436353618ae
Serban Balamaci <serban.balamaci@1and1.ro>
2015-10-27 14:37:23 +0200

    Move WEBDRIVER_BASE_URL from code in Cucumber Hook to serenity property file.

--- src/main/java/ro/fortsoft/pippo/demo/bdd/cucumber/hook/CucumberHooks.java
+++ src/main/java/ro/fortsoft/pippo/demo/bdd/cucumber/hook/CucumberHooks.java
@@ -1,7 +1,6 @@
 package ro.fortsoft.pippo.demo.bdd.cucumber.hook;
 
 import cucumber.api.java.Before;
-import net.thucydides.core.ThucydidesSystemProperty;
 import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.pages.Pages;
 import org.openqa.selenium.WebDriver;
@@ -18,17 +17,12 @@ public class CucumberHooks {
 
     @Before
     public void openBrowser() {
-        System.out.println("Opening browser....");
 //        Serenity.useFirefoxProfile(customFirefoxProfile());
+/*
         pages.getConfiguration().getEnvironmentVariables()
                 .setProperty(ThucydidesSystemProperty.WEBDRIVER_BASE_URL.getPropertyName(),
                         "http://tomcat:8080/pippo/");
-/*
-        pages.getConfiguration().getEnvironmentVariables()
-                .setProperty(ThucydidesSystemProperty.WEBDRIVER_REMOTE_URL.getPropertyName(),
-                        "http://tomcat:8080/pippo");
 */
-
 
         pages.getDriver().manage().deleteAllCookies();
         pages.getDriver().manage().window().maximize();
